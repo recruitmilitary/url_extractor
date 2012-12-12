62d0b9160bb44f6064e398fc40e5084be8dbfecd
Michael Guterl <michael@diminishing.org>
2012-12-12 11:54:10 -0500

    Autload VERSION constant to avoid warnings

--- lib/url_extractor.rb
+++ lib/url_extractor.rb
@@ -1,6 +1,5 @@
-require 'url_extractor/version'
-
 module UrlExtractor
+  autoload :VERSION, 'url_extractor/version'
   autoload :Util, 'url_extractor/util'
   autoload :PlainText, 'url_extractor/plain_text'
   autoload :HTML, 'url_extractor/html'
